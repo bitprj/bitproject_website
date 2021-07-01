a89aef46c66d4670a78318127250c1a517579f54
Daniel Kim <dgkim@ucdavis.edu>
2021-07-01 15:26:25 -0700

    Update index.jsx

--- components/textblock/index.jsx
+++ components/textblock/index.jsx
@@ -1,4 +1,18 @@
-import { Box, Stack, StackDivider, Heading, Container, Grid, GridItem } from '@chakra-ui/react'
+import {
+  Box,
+  Button,
+  Flex,
+  Heading,
+  Img,
+  Container,
+  Text,
+  Badge,
+  Grid,
+  Link,
+  GridItem,
+  useColorModeValue as mode,
+} from '@chakra-ui/react'
+
 import * as React from 'react'
 // import { Copyright } from './Copyright'
 import { LinkGrid } from './LinkGrid'
@@ -8,27 +22,24 @@ import { LinkGrid } from './LinkGrid'
 
 
 export const Textblock = ({ title, para1, para2 }) => (
-  // <Box
-  //   as="footer"
-  //   role="contentinfo"
-  //   mx="auto"
-  //   maxW="7xl"
-  //   py="12"
-  //   px={{
-  //     base: '4',
-  //      md: '8',
-  //   }}
-  // >
 
-  // </Box>
   <Box
     bg="black"
     color="white"
     >
     <Container
       maxW="container.lg"
+      p="15px"
     >
-    <Heading
+            <Box
+        maxW={{
+          base: 'xl',
+          md: '7xl',
+        }}
+        mx="auto"
+        
+      >
+      <Heading
       as="h1"
       size="3xl"
       fontWeight="extrabold"
@@ -38,87 +49,22 @@ export const Textblock = ({ title, para1, para2 }) => (
     >
       {title}
     </Heading>
-    
-    <Grid 
-    templateColumns="repeat(5, 1fr)" 
-    gap={4}
-    mt="2rem"
-    >
-      <GridItem colSpan={2}>{para1}</GridItem>
-      <GridItem colStart={3} colEnd={6}>{para2}</GridItem>
-    </Grid>
+        <Flex
+          align="flex-start"
+          direction={{
+            base: 'column',
+            lg: 'row',
+          }}
+          justify="space-between"
+          my="2rem"
+        > 
+          <Text mt="5" fontSize="xl">{para1}</Text>
+          <Text mt="5" fontSize="xl">{para2}</Text>
       {/* <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perferendis, error! Eaque quia natus ipsa suscipit cumque, praesentium cupiditate delectus architecto?</p> */}
+    </Flex>
+    </Box>
+    
     </Container>
   </Box>
 )
-
-
-
-// export const Textblock = ({ title, para1, para2 }) => (
-//   <Box
-//     as="footer"
-//     role="contentinfo"
-//     mx="auto"
-//     maxW="7xl"
-//     py="12"
-//     px={{
-//       base: '4',
-//        md: '8',
-//     }}
-//   >
-//     <Stack spacing="10" divider={<StackDivider />}>
-//       <Stack
-//         direction={{
-//           base: 'column',
-//           lg: 'row',
-//         }}
-//         spacing={{
-//           base: '10',
-//           lg: '28',
-//         }}
-//       >
-//         <Box flex="1">
-//           {/* <Logo /> */}
-//         </Box>
-//         {/* <Stack
-//           direction={{
-//             base: 'column',
-//             md: 'row',
-//           }}
-//           spacing={{
-//             base: '10',
-//             md: '20',
-//           }}
-//         > */}
-//           <LinkGrid
-//             spacing={{
-//               base: '10',
-//               md: '20',
-//               lg: '28',
-//             }}
-//             flex="1"
-//           />
-//           {/* <SubscribeForm
-//             width={{
-//               base: 'full',
-//               md: 'sm',
-//             }}
-//           /> */}
-//         </Stack>
-//       </Stack>
-//       {/* <Stack
-//         direction={{
-//           base: 'column-reverse',
-//           md: 'row',
-//         }}
-//         justifyContent="space-between"
-//         alignItems="center"
-//       >
-//         <Copyright />
-//         <SocialMediaLinks />
-//       </Stack> */}
-//     {/* </Stack> */}
-//   </Box>
-// )
-
 

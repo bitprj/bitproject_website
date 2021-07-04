4bd3bb0ffaeff6dc0c01e81a6248f0a1331647bd
emsesc <emseschen@gmail.com>
2021-07-03 23:37:09 -0400

    content edits 1
    no gifs yet :(

--- pages/serverless.js
+++ pages/serverless.js
@@ -22,7 +22,7 @@ export default function Serverless() {
       
       <Landing
       heading="Serverless Camp"
-      description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. 100% Free and Open Source"
+      description="As a student, master serverless functions to help you to build projects that solve real-world problems by integrating APIs, constructing user interfaces, and analyzing data."
       cta1="Apply Now"
       cta2="Learn More"
       image="/feature.svg"
@@ -32,16 +32,14 @@ export default function Serverless() {
       />
 
       <Textblock
-        title="Code. Deploy. That's it."
-        para1="You just write your code and deploy it. Everything else which is necessary to run your application is done automatically.
-        "
-        para2="You just write your code and deploy it. Everything else which is necessary to run your application is done automatically.
-        "
+        title="What is Serverless Camp?"
+        para1="Serverless involves harnessing the power of cloud computing and building large applications quickly without having to manage servers."
+        para2="Camp is an 8-week interactive coding bootcamp designed to help you gain real-world technical skills through project-based learning and build awesome professional connections."
       />
 
       <OverflowDualCol
         image="/emily.svg"
-        headerText="Emily built a file sharing app with Azure Functions and CosmosDB for AP exams"
+        headerText="Emily built a file sharing app with Azure Functions and Blob Storage for AP exams"
         action="Read Story &rarr;"
         actionLink="ACTIONLINK"
       />
@@ -61,8 +59,8 @@ export default function Serverless() {
       <DualCol 
         preheading="week 2"
         heading="Working with APIs"
-        para1="Build projects with awesome APIs from Twilio, Microsoft Cognitive Services, and Giphy! Learn how to read documentation, experiment, and most importantly Google!"
-        para2="Build projects to get started with Azure Functions, Cloud Engineering, and Javascript!"
+        para1="Learn how to read documentation, experiment, and most importantly Google."
+        para2="Build projects with awesome APIs from Twilio, Microsoft Cognitive Services, and Giphy!"
         li1="😊 Emotional Gifs"
         li2="🎶 Song4u.js"
         img="/counselorbot.svg"
@@ -71,12 +69,12 @@ export default function Serverless() {
       />
        <DualCol 
         preheading="week3"
-        heading="Git & Serverless Basics"
-        para1="Set up your development environment with developer tools like Github and Postman."
-        para2="Build projects to get started with Azure Functions, Cloud Engineering, and Javascript!"
+        heading="Storing & Retrieving Data"
+        para1="Learn how to interact with databases add exciting and functional features to your projects."
+        para2="Build projects with Blob Storage and a NoSQL database to store data received through your serverless function!"
         li1="⚡ HackerVoice API"
-        li2="🐱 twoCatz API"
-        li3="🐸 [TOP SECRET] API"
+        li2="🐰 Bunnimage API"
+        li3="🤫 Deep Secrets API"
         img="/counselorbot.svg"
         action="Get Started &rarr;"
         actionLink="ACTIONLINK"
@@ -95,7 +93,7 @@ export default function Serverless() {
       />
     <CenterDualCol
       heading="Build your Own Project"
-      description="Set up your development environment with developer tools like Github and Postman."
+      description="Demonstrate your new competence with cloud computing by developing your own project: it can solve problems, be fun, or do all of the above!"
       cta1="Sample Projects"
       cta2="Learn More"
       image="/feature.svg"

39d2640a6111f0419321f51ed8e15589c34a4afd
emsesc <emseschen@gmail.com>
2021-07-04 12:45:39 -0400

    finish gifs :100:

--- pages/serverless.js
+++ pages/serverless.js
@@ -63,7 +63,7 @@ export default function Serverless() {
         para2="Build projects with awesome APIs from Twilio, Microsoft Cognitive Services, and Giphy!"
         li1="😊 Emotional Gifs"
         li2="🎶 Song4u.js"
-        img="/week1.gif"
+        img="/week2.gif"
         action="Get Started &rarr;"
         actionLink="ACTIONLINK"
       />
@@ -74,7 +74,7 @@ export default function Serverless() {
         para2="Build projects with Blob Storage and a NoSQL database to store data received through your serverless function!"
         li1="🐰 Bunnimage API"
         li2="🤫 Deep Secrets API"
-        img="/week1.gif"
+        img="/week3.gif"
         action="Get Started &rarr;"
         actionLink="ACTIONLINK"
       />
@@ -85,7 +85,7 @@ export default function Serverless() {
         para2="Build a frontend with HTML/CSS/JS to interact with your serverless functions!"
         li1="🐇 Bunnimage: The App"
         li2="🐈 twoCatz: The App"
-        img="/week1.gif"
+        img="/week4.gif"
         action="Get Started &rarr;"
         actionLink="ACTIONLINK"
       />

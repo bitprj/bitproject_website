13708f2f3ca141468bd89d5a36b507bf877a9ad7
Ganning Xu <ganningxu@gmail.com>
2022-04-13 23:03:34 -0400

    added sponsor redirect

---
+++ pages/sponsor.js
@@ -0,0 +1,19 @@
+import Head from "next/head";
+import React, { useState } from "react";
+
+export default function Serverless2022() {
+  return (
+    <div>
+      <Head>
+        <title>Sponsor</title>
+        <meta
+          httpEquiv="refresh"
+          content="0;URL='https://plucky-singer-a26.notion.site/Serverless-Camp-Sponsorship-Prospectus-1659e155cc0c4c03bb16d865e1feed5f'"
+        />
+        <meta property="og:site_name" content="bitproject.org" />
+        <meta property="og:type" content="website" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </div>
+  );
+}

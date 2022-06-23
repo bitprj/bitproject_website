af38c4c3a46dc63a90089166d1b7f252eda5fd59
Ganning Xu <ganningxu@gmail.com>
2022-06-23 15:42:42 -0400

    revert index.js text

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,42 @@
-import Head from 'next/head'
-import { Landing } from '../components/landing.jsx'
-import { TriCol } from '../components/tricol'
-import { GraphicTab } from '../components/graphictab'
-import { Testimonials } from '../components/testimonials'
-import { Navbar } from '../components/navbar'
-import { Footer } from '../components/footer'
-import { buildUrl } from 'cloudinary-build-url';
-
+import Head from "next/head";
+import { Landing } from "../components/landing.jsx";
+import { TriCol } from "../components/tricol";
+import { GraphicTab } from "../components/graphictab";
+import { Testimonials } from "../components/testimonials";
+import { Navbar } from "../components/navbar";
+import { Footer } from "../components/footer";
+import { buildUrl } from "cloudinary-build-url";
 
 export default function HomePage() {
-  const url = buildUrl('/main.gif', {
+  const url = buildUrl("/main.gif", {
     cloud: {
-      cloudName: 'dozq22ont',
+      cloudName: "dozq22ont",
     },
     transformations: {
       effect: {
-        name: 'pixelate',
-        value: 40
-      }
-    }
+        name: "pixelate",
+        value: 40,
+      },
+    },
   });
 
   return (
     <div>
       <Head>
         <title>Bit Project</title>
-        <meta name="description" content="We provide cutting-edge, open source learning experiences to prepare students for careers in tech." />
+        <meta
+          name="description"
+          content="We provide cutting-edge, open source learning experiences to prepare students for careers in tech."
+        />
         <meta property="og:title" content="Bit Project" />
-        <meta property="og:image" content="https://www.bitproject.org/_next/image?url=%livecode.png&w=2048&q=75" />
-        <meta property="og:description" content="We provide cutting-edge, open source learning experiences to prepare students for careers in tech." />
+        <meta
+          property="og:image"
+          content="https://www.bitproject.org/_next/image?url=%livecode.png&w=2048&q=75"
+        />
+        <meta
+          property="og:description"
+          content="We provide cutting-edge, open source learning experiences to prepare students for careers in tech."
+        />
         <meta property="og:url" content="https://www.bitproject.org/" />
         <meta property="og:site_name" content="bitproject.org" />
         <meta property="og:type" content="website" />
@@ -36,7 +44,7 @@ export default function HomePage() {
       </Head>
       <Navbar />
       <Landing
-        heading="test"
+        heading="We equip students with technical superpowers"
         description="We provide cutting-edge, open source learning experiences to prepare students for careers in tech."
         formActionButton="Sign Up"
         image="/main.gif"
@@ -59,18 +67,14 @@ export default function HomePage() {
         name1="Fifi Teklemedhin"
         title1="11th Grade"
         graphic1="https://vimeo.com/570820559"
-
         pic2="/anita.jpeg"
         name2="Anita Padman" // need to change to promo video. this one is about serverless
         title2="Junior at UIC"
         graphic2="https://vimeo.com/572033757"
-
-
         pic3="/emily.png"
         name3="Emily Chen"
         title3="11th Grade"
         graphic3="https://vimeo.com/572255574"
-
       />
 
       <Testimonials
@@ -79,7 +83,6 @@ export default function HomePage() {
       />
 
       <Footer />
-
     </div>
-  )
+  );
 }
